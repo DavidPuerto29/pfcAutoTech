fd95deaf62a66c731d47aabafc148161945e64cc
David <David@DESKTOP-TLODCFR>
2025-03-20 16:45:04 +0100

    Primer commit

---
+++ AutoTech/src/main/java/davidpuertocuenca/autotech/AutoTech.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package davidpuertocuenca.autotech;
+
+import davidpuertocuenca.autotech.vistas.login.LoginClientes;
+
+/**
+ *
+ * @author David
+ */
+public class AutoTech {
+
+    public static void main(String[] args) {
+        LoginClientes test = new LoginClientes();
+            test.setVisible(true);
+    }
+}
